acba6ed827a0dfbf2a39ac4daded73dd46e6a202
xenoturbellida <ElPGordeeva@stud.kpfu.ru>
2021-11-02 19:34:01 +0300

    task 2 js, не законченная

---
+++ js_tasks/task_2.js
@@ -0,0 +1,15 @@
+function task2 (word1, word2) {
+    let word1_letters = [];
+    for (let letter of word1) {
+        if (letter in word1_letters) {
+            continue
+        }
+        word1_letters.push(letter);
+    }
+
+    let word1_letters_number = []
+
+    for (let letter of word2) {
+        word2_letters.push(letter);
+    }
+}
